f4bd5bb37e69560d34137350a3a6846fda167be6
tharamaetunacao <tunacao.tharamae@cec.edu.ph>
2025-11-29 20:33:26 +0800

    Update3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // important for Vercel
-  },
-})
+  base: './', // <- important: use relative path for Vercel
+});
